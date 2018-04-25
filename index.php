070f30ec37d8b549511edf60d162a44398e65b49
schoenked <33178062+schoenked@users.noreply.github.com>
2018-04-25 23:20:21 +0200

    first upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <title>Nr-Anzeige im Versammlungsraum</title>
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="js/bootstrap.min.js"></script>
+
+</head>
+<body>
+
+<div class="navbar navbar-inverse navbar-fixed-top">
+    <h3 class="navbar-header navbar-brand" >Mennoniten Brüdergemeinde Neuwied-Gladbach</h3>
+</div>
+<div class="container-fluid">
+    <h2 class="left">Nummernanzeige im Versammlungsraum</h2>
+    <p>This is some text.</p>
+</div>
+</body>
+<?php
+?>
+</html>
