b96b6c1e72e198124fc55f4892459d39b4c7a088
schoenked <33178062+schoenked@users.noreply.github.com>
2018-04-26 00:49:14 +0200

    UI implemented

--- index.php
+++ index.php
@@ -5,19 +5,42 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <title>Nr-Anzeige im Versammlungsraum</title>
 
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <script src="js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/w3.css">
+    <script src="js/sender.js"></script>
 
 </head>
 <body>
+<div class="w3-padding-small w3-right w3-margin-right w3-black">
+    <img src="img/violinschluessel.png">
+</div>
+<div class="w3-bar">
 
-<div class="navbar navbar-inverse navbar-fixed-top">
-    <h3 class="navbar-header navbar-brand" >Mennoniten Brüdergemeinde Neuwied-Gladbach</h3>
+    <div>
+        <h5 class="w3-border-bottom w3-border-indigo w3-margin-left w3-text-brown">Mennoniten Brüdergemeinde
+            Neuwied-Gladbach</h5>
+    </div>
 </div>
-<div class="container-fluid">
-    <h2 class="left">Nummernanzeige im Versammlungsraum</h2>
-    <p>This is some text.</p>
+<div class="w3-display-container w3-content w3-padding-small bottom">
+    <h3 class="left">Nummernanzeige im Versammlungsraum</h3>
+    <div class="w3-margin-top">Singen wir noch das Lied unter der</div>
+    <h3 class="w3-text-indigo">Nummer</h3>
+    <div class="" style="white-space: nowrap;">
+        <input type="number" min="0"
+               class="w3-xlarge w3-wide w3-padding-24 w3-border-bottom w3-border-brown w3-border-0 w3-left-align w3-sand w3-text-indigo">
+        <button class="w3-round  w3-button w3-btn w3-grey">
+            <img class="w3-right-align w3-padding-16" src="img/send.png"
+        </button>
+    </div>
+
 </div>
+<div class="w3-margin">
+<br>
+</div>
+<div id="response" class="w3-card-2 w3-green w3-text-white w3-bottom">
+    <div class="w3-margin-left w3-margin">
+        Bereit
+    </div>
+</div></div>
 </body>
 <?php
 ?>
